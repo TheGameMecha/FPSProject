6558493b0c255baa58eee524631d2913d7384ee7
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2021-05-12 23:41:04 -0400

    Setting up initial C++ classes

---
+++ Source/FPSProject/FPSCharacter.cpp
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationYaw = false;
+
+	// Create a First Person Camera component
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(FPSCameraComponent != nullptr);
+
+	// Attach the camera component to the capsule component
+	FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+
+	// Position the camera slightly above the eyes.
+	FPSCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+
+	// Enable the pawn to control camera rotation.
+	FPSCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a first person mesh component for the owning player
+	FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(FPSMesh != nullptr);
+
+	// Only the owning player sees this mesh
+	FPSMesh->SetOnlyOwnerSee(true);
+
+	FPSMesh->SetupAttachment(FPSCameraComponent);
+
+	// Disable some environmental shadows to preserve the illusion of having a single mesh
+	FPSMesh->bCastDynamicShadow = false;
+	FPSMesh->CastShadow = false;
+
+	// The owning player doesn't see the regular (third-person) body mesh.
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	check(GEngine != nullptr);
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Set up Movement bindings
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+
+	// Set up Look bindings
+	PlayerInputComponent->BindAxis("Turn", this, &AFPSCharacter::CameraTurn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AFPSCharacter::CameraLookUp);
+
+	// Set up "Action" bindings
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPSCharacter::StopJump);
+}
+
+void AFPSCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void AFPSCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void AFPSCharacter::CameraTurn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AFPSCharacter::CameraLookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AFPSCharacter::StopJump()
+{
+	bPressedJump = false;
+}
---
+++ Source/FPSProject/FPSCharacter.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "FPSCharacter.generated.h"
+
+UCLASS()
+class FPSPROJECT_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	UFUNCTION()
+		void MoveRight(float Value);
+
+	UFUNCTION()
+		void CameraTurn(float Value);
+
+	UFUNCTION()
+		void CameraLookUp(float Value);
+
+	UFUNCTION()
+		void StartJump();
+
+	UFUNCTION()
+		void StopJump();
+
+	// FPS Camera
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* FPSCameraComponent;
+
+	// First-person mesh (arms) visible only to owning player
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* FPSMesh;
+};
